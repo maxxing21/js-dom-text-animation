0ed2afcf82426a2231972e053dda5ca6c51a9963
maxxing21 <katrinehj@outlook.com>
2026-09-24 16:51:18 +0200

    updated getZoomBtn

--- js/script.js
+++ js/script.js
@@ -5,18 +5,20 @@
 const getWordElem = document.getElementById("word");
 
 // Skriv selv: hent knappen "zoomBtn" på samme måde, ved hjælp af dens id. Variablen skal hedde getZoomBtn
-const getZoombtn = document.getElementById("zoomBtn");
+const getZoomBtn = document.getElementById("zoomBtn");
 
 // Eksempel: vi lytter efter klik på knappen og kører en anonym function, når der klikkes - ligesom i billedskift-opgaven
 getZoomBtn.addEventListener("click", function() {
 
     // Skriv if/else-strukturen selv herinde, ligesom i billedskift-opgaven.
-    if(this.textContent === true){
-       getWordElem.style.fontSize = `6rem`;
-       this.textContent = `Zoom ud`
-    }else
-        getWordElem.style.fontSize = `3rem`;
+    if(this.textContent === `Zoom ind`) {
+        getWordElem.style.fontSize = `6rem`;
+        this.textContent = `Zoom ud`
+
+    }  else{
+        getWordElem.style.fontSize= `3rem`;
         this.textContent = `Zoom ind`
+    }
 
     // Nyt i dag: getWordElem.style.fontSize ændrer en CSS-egenskab (fontSize) direkte via JavaScript,
     // på samme måde som getImage.src ændrede et billede i billedskift-opgaven.
@@ -27,5 +29,7 @@ getZoomBtn.addEventListener("click", function() {
     // Ellers:
     //   - sæt getWordElem.style.fontSize til "3rem"
     //   - sæt this.textContent til "Zoom ind"
+
+    
 
 });
